e0cf7a11576a8a3dcc7ce9d22c9423d528531466
Shreeramgopal Sambandam <shreeramgopal.sambandam@psiog.com>
2025-04-20 20:47:28 +0530

    Cypress configuration

---
+++ cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
+    },
+    baseUrl: 'http://localhost:3000',
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  },
+  component: {
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
+    },
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
+    supportFile: 'cypress/support/component.js',
+    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
+  },
+});
